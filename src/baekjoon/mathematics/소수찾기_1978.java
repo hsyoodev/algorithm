cb7b15cb0ab19d38bbf62d8dfd80c2855f5f4757
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-08-05 12:35:43 +0900

    modified 1978

--- src/baekjoon/mathematics/소수찾기_1978.java
+++ src/baekjoon/mathematics/소수찾기_1978.java
@@ -2,27 +2,24 @@ package baekjoon.mathematics;
 
 import java.util.Scanner;
 
-public class GCD1_11689 {
+public class 소수찾기_1978 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] A = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            A[i] = i;
-        }
-        for (int i = 2; i <= n; i++) {
-            if (A[i] == i) {
-                if ()
-                for (int j = i; j <= n; j += i) {
-                    A[j] -= (A[j] / i);
+        int N = sc.nextInt();
+        int answer = N;
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (num == 1) {
+                answer--;
+            } else {
+                for (int j = 2; j <= Math.sqrt(num) ; j++) {
+                    if (num % j == 0) {
+                        answer--;
+                        break;
+                    }
                 }
             }
         }
-//        for (int i = 2; i <= n; i++) {
-//            if (n % i == 0) {
-//                n -= (n / i);
-//            }
-//        }
-        System.out.print(A[n]);
+        System.out.print(answer);
     }
 }
