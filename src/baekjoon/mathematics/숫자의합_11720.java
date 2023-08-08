ac0833b3ef0a43f6bf75ebfb01207dcba7f46b5a
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-08-08 11:55:25 +0900

    modified 11720

--- src/baekjoon/mathematics/숫자의합_11720.java
+++ src/baekjoon/mathematics/숫자의합_11720.java
@@ -1,2 +1,11 @@
-package baekjoon.mathematics;public class 숫자의합_11720 {
+package baekjoon.mathematics;
+
+import java.util.Scanner;
+
+public class 숫자의합_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        System.out.print(sc.next().chars().map(i -> i - '0').sum());
+    }
 }
