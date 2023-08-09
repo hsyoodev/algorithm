c474769b768f0bd29e73c0da063dcecc0545773e
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-08-09 13:50:24 +0900

    solved 11659

---
+++ src/baekjoon/mathematics/구간합구하기4_11659.java
@@ -0,0 +1,26 @@
+package baekjoon.mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간합구하기4_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] intArr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            intArr[i] = intArr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        for (int k = 0; k < M; k++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            System.out.println(intArr[j] - intArr[i - 1]);
+        }
+    }
+}
