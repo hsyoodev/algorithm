61061a85fc0705233dedb45f1adc230985556aac
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-08-12 01:00:36 +0900

    solved 2018

---
+++ src/baekjoon/prefix_sum/수들의합5_2018.java
@@ -0,0 +1,25 @@
+package baekjoon.prefix_sum;
+
+import java.util.Scanner;
+
+public class 수들의합5_2018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 1;
+        int sum = 0;
+        int start = 1;
+        for (int i = 1; i <= N; i++) {
+            if (sum > N) {
+                sum -= start++;
+                i--;
+                continue;
+            } else if (sum == N){
+                sum -= start++;
+                count++;
+            }
+            sum += i;
+        }
+        System.out.print(count);
+    }
+}
