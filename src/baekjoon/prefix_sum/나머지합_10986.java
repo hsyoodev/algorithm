ae9fb229435660d17075f97d466ed760ffd11ccc
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-08-14 17:07:28 +0900

    checked 10986

--- src/baekjoon/prefix_sum/나머지합_10986.java
+++ src/baekjoon/prefix_sum/나머지합_10986.java
@@ -3,46 +3,33 @@ package baekjoon.prefix_sum;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class 나머지합_10986 {
-    private static int[] intArr;
-    private static int count = 0;
-    private static Set<String> check = new HashSet<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        intArr = new int[N + 1];
+        long[] sumArr = new long[N + 1];
+        long[] cntArr = new long[M];
+        long answer = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i < N + 1; i++) {
-            int A = Integer.parseInt(st.nextToken());
-            if (A % M == 0) {
-                count++;
+            sumArr[i] = sumArr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i < N + 1; i++) {
+            int divNum = (int) (sumArr[i] % M);
+            if (divNum == 0) {
+                answer++;
             }
-            intArr[i] = intArr[i - 1] + A;
+            cntArr[divNum]++;
         }
-        test(1, N, intArr[N], M);
-        System.out.print(count);
-    }
-    private static void test(int start, int end, int sum, int num) {
-        if (start == end) {
-            return;
-        }
-        if (sum % num == 0) {
-            count++;
-        }
-        if (!check.contains(start + "" + end)){
-            check.add(start + "" + end);
-            if (!check.contains((start + 1) + "" + end)){
-                test(start + 1, end, intArr[end] - intArr[start], num);
-            }
-            if (!check.contains(start + "" + (end - 1))){
-                test(start, end - 1, intArr[end - 1] - intArr[start - 1], num);
+        for (int i = 0; i < M; i++) {
+            if (cntArr[i] > 1) {
+                answer += (cntArr[i] * (cntArr[i] - 1) / 2);
             }
         }
+        System.out.print(answer);
     }
 }
