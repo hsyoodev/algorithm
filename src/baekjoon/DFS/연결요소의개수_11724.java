b5eee5423965e71a8c79d90261a20926a9456042
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-09-15 20:40:20 +0900

    modified 11724

--- src/baekjoon/DFS/연결요소의개수_11724.java
+++ src/baekjoon/DFS/연결요소의개수_11724.java
@@ -7,57 +7,33 @@ public class 연결요소의개수_11724 {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int M = scanner.nextInt();
-        if (M == 0) {
-            System.out.print(0);
-            return;
-        }
         Queue<Integer>[] adjacencyList = new Queue[N + 1];
         for (int i = 1; i < N + 1; i++) {
             adjacencyList[i] = new LinkedList<>();
         }
+        for (int i = 0; i < M; i++) {
+            int node = scanner.nextInt();
+            int adjacencyPoint = scanner.nextInt();
+            adjacencyList[node].offer(adjacencyPoint);
+            adjacencyList[adjacencyPoint].offer(node);
+        }
         boolean[] visited = new boolean[N + 1];
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < M; i++) {
-            int currentPoint = scanner.nextInt();
-            int adjacencyPoint = scanner.nextInt();
-            if (i == 0) {
-                visited[currentPoint] = true;
-                stack.push(currentPoint);
-            }
-            adjacencyList[currentPoint].offer(adjacencyPoint);
-        }
-        int answer = 1;
-        while (!stack.isEmpty()) {
-            int currentPoint = stack.pop();
-            while (!adjacencyList[currentPoint].isEmpty()) {
-                int adjacencyPoint = adjacencyList[currentPoint].poll();
-                if (!visited[adjacencyPoint]) {
-                    stack.push(adjacencyPoint);
-                    visited[adjacencyPoint] = true;
-                } else {
-                    boolean a = true;
-                    for (int i = 1; i < N + 1; i++) {
-                        if (!visited[i]) {
-                            a = false;
+        int answer = 0;
+        for (int i = 1; i < N + 1; i++) {
+            if (!visited[i]) {
+                answer++;
+                stack.push(i);
+                visited[i] = true;
+                while (!stack.isEmpty()) {
+                    int node = stack.pop();
+                    while (!adjacencyList[node].isEmpty()) {
+                        int adjacencyPoint = adjacencyList[node].poll();
+                        if (!visited[adjacencyPoint]) {
+                            stack.push(adjacencyPoint);
+                            visited[adjacencyPoint] = true;
                         }
                     }
-                    if (a) {
-                        answer++;
-                    }
-                }
-            }
-            if (stack.isEmpty()) {
-                boolean a = true;
-                for (int i = 1; i < N + 1; i++) {
-                    if (!visited[i]) {
-                        stack.push(i);
-                        visited[i] = true;
-                        a = false;
-                        break;
-                    }
-                }
-                if (a) {
-                    answer++;
                 }
             }
         }
