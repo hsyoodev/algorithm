40de6629ce1d45a68ee8e2c3ab5d7c2a3a6d2e01
hsyoodev <102946491+hsyoodev@users.noreply.github.com>
2023-09-17 18:12:05 +0900

    modified 1517

--- src/baekjoon/sorting/버블소트_1517.java
+++ src/baekjoon/sorting/버블소트_1517.java
@@ -7,15 +7,14 @@ import java.util.StringTokenizer;
 
 public class 버블소트_1517 {
     private static int[] A;
-    private static sData[] M;
+    private static int[] M;
     private static long swap = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         A = new int[N + 1];
-        M = new sData[N + 1];
+        M = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
@@ -38,41 +37,26 @@ public class 버블소트_1517 {
         while (m <= e) {
             if (mv1 < s2 && mv2 <= e) {
                 if (A[mv1] > A[mv2]) {
-                    M[m] = new sData(A[mv2], mv2);
+                    M[m] = A[mv2];
+                    swap += mv2 - m;
                     mv2++;
                 } else {
-                    M[m] = new sData(A[mv1], mv1);
+                    M[m] = A[mv1];
                     mv1++;
                 }
             } else {
                 if (mv1 >= s2 && mv2 <= e) {
-                    M[m] = new sData(A[mv2], mv2);
+                    M[m] = A[mv2];
                     mv2++;
                 } else {
-                    M[m] = new sData(A[mv1], mv1);
+                    M[m] = A[mv1];
                     mv1++;
                 }
             }
             m++;
         }
-
-        int diff = 0;
         for (int j = s; j <= e; j++) {
-            diff = M[j].index - j;
-            if (diff > 0) {
-                swap += diff;
-            }
-            A[j] = M[j].value;
+            A[j] = M[j];
         }
-    }
-}
-
-class sData {
-    int value;
-    int index;
-
-    public sData(int value, int index) {
-        this.value = value;
-        this.index = index;
     }
 }
