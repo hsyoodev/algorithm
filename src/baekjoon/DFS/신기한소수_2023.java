4af3a73426d3b87ea59550c41428a9bf301153b1
hsyoodev <hsyoodev@gmail.com>
2023-09-19 11:20:15 +0900

    checked 2023

--- src/baekjoon/DFS/신기한소수_2023.java
+++ src/baekjoon/DFS/신기한소수_2023.java
@@ -3,26 +3,32 @@ package baekjoon.DFS;
 import java.util.Scanner;
 
 public class 신기한소수_2023 {
+    private static int N;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int endNumber = (int) Math.pow(10, N) - 1;
-        int startNumber = (endNumber + 1) / 5;
-        for (int i = startNumber; i <=  endNumber; i++) {
-            if (isPrimeNumber(i)) {
-                System.out.println(i);
+        N = scanner.nextInt();
+        printAmazingPrimeNumber(2, 1);
+        printAmazingPrimeNumber(3, 1);
+        printAmazingPrimeNumber(5, 1);
+        printAmazingPrimeNumber(7, 1);
+    }
+    private static void printAmazingPrimeNumber(int number, int digit) {
+        if (digit == N) {
+            System.out.println(number);
+            return;
+        }
+        for (int i = 1; i < 10; i += 2) {
+            if (isPrimeNumber(number * 10 + i)) {
+                printAmazingPrimeNumber(number * 10 + i, digit + 1);
             }
         }
     }
-    public static boolean isPrimeNumber(int number){
-        if (number == 0) {
-            return true;
-        }
+    private static boolean isPrimeNumber(int number) {
         for (int i = 2; i * i <= number; i++) {
             if (number % i == 0) {
                 return false;
             }
         }
-        return isPrimeNumber(number / 10);
+        return true;
     }
 }
