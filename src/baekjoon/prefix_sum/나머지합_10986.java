139c0d5628aa544dc0d1e8917a10d7aeed27ecd7
hsyoodev <hsyoodev@gmail.com>
2023-09-20 11:47:38 +0900

    refactored 10986

--- src/baekjoon/prefix_sum/나머지합_10986.java
+++ src/baekjoon/prefix_sum/나머지합_10986.java
@@ -6,30 +6,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 나머지합_10986 {
+    private static long answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        long[] sumArr = new long[N + 1];
-        long[] cntArr = new long[M];
-        long answer = 0;
+        long[] prefixSum = new long[N + 1];
+        long[] remainderCount = new long[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i < N + 1; i++) {
-            sumArr[i] = sumArr[i - 1] + Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(st.nextToken());
         }
         for (int i = 1; i < N + 1; i++) {
-            int divNum = (int) (sumArr[i] % M);
-            if (divNum == 0) {
+            int remainder = (int) (prefixSum[i] % M);
+            if (remainder == 0) {
                 answer++;
             }
-            cntArr[divNum]++;
+            remainderCount[remainder]++;
         }
+        combination(remainderCount, M);
+        System.out.print(answer);
+    }
+    public static void combination(long[] remainderCount, int M) {
+        // remainderCount[i]개의 원소를 갖는 집합에서 2개의 원소를 선택
         for (int i = 0; i < M; i++) {
-            if (cntArr[i] > 1) {
-                answer += (cntArr[i] * (cntArr[i] - 1) / 2);
+            if (remainderCount[i] > 1) {
+                answer += (remainderCount[i] * (remainderCount[i] - 1) / 2);
             }
         }
-        System.out.print(answer);
     }
 }
