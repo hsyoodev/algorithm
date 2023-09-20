69ad331ec0ed7ce8640f14d8cadb0dd8243f34e1
hsyoodev <hsyoodev@gmail.com>
2023-09-20 10:14:35 +0900

    refactored 11659

--- src/baekjoon/prefix_sum/구간합구하기4_11659.java
+++ src/baekjoon/prefix_sum/구간합구하기4_11659.java
@@ -3,6 +3,7 @@ package baekjoon.prefix_sum;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 구간합구하기4_11659 {
@@ -12,15 +13,15 @@ public class 구간합구하기4_11659 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int[] intArr = new int[N + 1];
+        int[] prefixSum = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            intArr[i] = intArr[i - 1] + Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(st.nextToken());
         }
         for (int k = 0; k < M; k++) {
             st = new StringTokenizer(br.readLine());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
-            System.out.println(intArr[j] - intArr[i - 1]);
+            System.out.println(prefixSum[j] - prefixSum[i - 1]);
         }
     }
 }
