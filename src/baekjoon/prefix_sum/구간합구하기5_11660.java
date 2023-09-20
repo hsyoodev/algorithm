ecd8f8739a5c59a2e752243ea5cd84963739d609
hsyoodev <hsyoodev@gmail.com>
2023-09-20 10:41:46 +0900

    refactored 11660

--- src/baekjoon/prefix_sum/구간합구하기5_11660.java
+++ src/baekjoon/prefix_sum/구간합구하기5_11660.java
@@ -12,10 +12,10 @@ public class 구간합구하기5_11660 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] table = new int[N + 1][N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        for (int x = 1; x < N + 1; x++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < N + 1; j++) {
-                table[i][j] = Integer.parseInt(st.nextToken()) + table[i - 1][j] + table[i][j - 1] - table[i - 1][j - 1];
+            for (int y = 1; y < N + 1; y++) {
+                table[x][y] = Integer.parseInt(st.nextToken()) + table[x - 1][y] + table[x][y - 1] - table[x - 1][y - 1];
             }
         }
         for (int i = 0; i < M; i++) {
@@ -24,9 +24,6 @@ public class 구간합구하기5_11660 {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            // 이전 코드
-            // System.out.println(table[x2][y2] - table[x1 - 1][y2] - table[x2][y1 - 1] + 1);
-            // 정답 코드
             System.out.println(table[x2][y2] - table[x1 - 1][y2] - table[x2][y1 - 1] + table[x1 - 1][y1 - 1]);
         }
     }
