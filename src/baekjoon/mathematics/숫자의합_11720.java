9e8cd83c25c882a5e08585abefce9ad4fe73ea91
hsyoodev <hsyoodev@gmail.com>
2023-09-20 09:33:17 +0900

    refactored 11720

--- src/baekjoon/mathematics/숫자의합_11720.java
+++ src/baekjoon/mathematics/숫자의합_11720.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class 숫자의합_11720 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        System.out.print(sc.next().chars().map(i -> i - '0').sum());
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        int sum = scanner.next().chars()
+                .map(i -> i - '0')
+                .sum();
+        System.out.print(sum);
     }
 }
