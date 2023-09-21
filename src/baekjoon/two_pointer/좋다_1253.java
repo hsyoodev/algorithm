ee0259dcdf40dfe766a802ba7c724b1362908eb0
hsyoodev <hsyoodev@gmail.com>
2023-09-21 10:02:58 +0900

    refactored 1253

--- src/baekjoon/two_pointer/좋다_1253.java
+++ src/baekjoon/two_pointer/좋다_1253.java
@@ -9,41 +9,31 @@ import java.util.StringTokenizer;
 public class 좋다_1253 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 수의 개수
         int N = Integer.parseInt(br.readLine());
-        // N개의 숫자들 A
-        int[] A = new int[N];
-        // 문자열 공백 단위로 나누기
+        int[] numbers = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // A 초기화
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-        // A 오름차순 정렬
-        Arrays.sort(A);
-        // 다른 수 두개의 합
-        int sum = 0;
-        // 좋은 수의 개수
+        Arrays.sort(numbers);
         int answer = 0;
         for (int i = 0; i < N; i++) {
-            // 시작 인덱스
-            int start = 0;
-            // 끝 인덱스
-            int end = N - 1;
-            while (start < end) {
-                if (start == i) {
-                    start++;
-                } else if (end == i) {
-                    end--;
+            int startIndex = 0;
+            int endIndex = N - 1;
+            while (startIndex < endIndex) {
+                if (startIndex == i) {
+                    startIndex++;
+                } else if (endIndex == i) {
+                    endIndex--;
                 } else {
-                    sum = A[start] + A[end];
-                    if (sum < A[i]) {
-                        start++;
-                    } else if (sum == A[i]) {
+                    int sum = numbers[startIndex] + numbers[endIndex];
+                    if (sum < numbers[i]) {
+                        startIndex++;
+                    } else if (sum == numbers[i]) {
                         answer++;
                         break;
                     } else {
-                        end--;
+                        endIndex--;
                     }
                 }
             }
