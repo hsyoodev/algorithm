7a67d4a4694e2966260cbe8727cd1d98d578e356
hsyoodev <hsyoodev@gmail.com>
2023-09-21 10:48:43 +0900

    refactored 12891

--- src/baekjoon/sliding_window/DNA비밀번호_12891.java
+++ src/baekjoon/sliding_window/DNA비밀번호_12891.java
@@ -6,50 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class DNA비밀번호_12891 {
-    // 부분문자열에 포함되어야 할 {'A', 'C', 'G', 'T'}의 최소 개수 R
-    private static int[] R = new int[4];
-    // 부분문자열의 {'A', 'C', 'G', 'T'}의 개수 V
-    private static int[] V = new int[4];
-    // 비밀번호의 종류의 수 answer
+    // 부분문자열에 포함되어야 할 {'A', 'C', 'G', 'T'} 의 최소 개수
+    private static int[] minCounts = new int[4];
+    // 부분문자열의 {'A', 'C', 'G', 'T'}의 개수
+    private static int[] counts = new int[4];
     private static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // DNA 문자열의 길이 S
         int S = Integer.parseInt(st.nextToken());
-        // 부분문자열의 길이 M
         int P = Integer.parseInt(st.nextToken());
-        // DNA 문자열 DNA
-        String DNA = br.readLine();
+        char[] DNA = br.readLine().toCharArray();
         st = new StringTokenizer(br.readLine());
-        // R 초기화
         for (int i = 0; i < 4; i++) {
-            R[i] = Integer.parseInt(st.nextToken());
+            minCounts[i] = Integer.parseInt(st.nextToken());
         }
-        // 첫번째 0 ~ M 부분문자열 {'A', 'C', 'G', 'T'}의 개수 초기화
+        // 첫 번째 0 ~ (P - 1) 부분문자열 검증
         for (int i = 0; i < P; i++) {
-            V[getIndex(DNA.charAt(i))]++;
+            counts[getIndex(DNA[i])]++;
         }
-        // 첫번째 0 ~ M 부분문자열 검증
         verify();
-        // 시작 인덱스
-        int start = 0;
-        // 끝 인덱스
-        int end = P - 1;
-        // 두번째부터 앞뒤부분만 -, +하면서 부분문자열 검증
+        // 두 번째부터 앞뒤 부분만 -, +하면서 부분문자열 검증
+        int startIndex = 0;
+        int endIndex = P - 1;
         for (int i = 1; i <= S - P; i++) {
-            // 앞부분 -
-            V[getIndex(DNA.charAt(start++))]--;
-            // 뒷부분 +
-            V[getIndex(DNA.charAt(++end))]++;
-            // start ~ end 부분문자열 검증
+            counts[getIndex(DNA[startIndex++])]--;
+            counts[getIndex(DNA[++endIndex])]++;
             verify();
         }
         System.out.print(answer);
     }
-    // {'A', 'C', 'G', 'T'}의 index 반환
-    public static int getIndex(char c) {
+
+    private static int getIndex(char c) {
+        // {'A', 'C', 'G', 'T'}의 index 반환
         int index;
         if (c == 'A') {
             index = 0;
@@ -62,11 +52,12 @@ public class DNA비밀번호_12891 {
         }
         return index;
     }
-    // {'A', 'C', 'G', 'T'} 검증
-    public static void verify() {
+
+    private static void verify() {
+        // {'A', 'C', 'G', 'T'} 검증
         int count = 0;
         for (int j = 0; j < 4; j++) {
-            if (R[j] <= V[j]) {
+            if (minCounts[j] <= counts[j]) {
                 count++;
             }
         }
