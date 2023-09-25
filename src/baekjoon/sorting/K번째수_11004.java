591ca3c7e262328b993df081fcd071ca08294a61
hsyoodev <hsyoodev@gmail.com>
2023-09-25 13:58:43 +0900

    unsolved 11004

--- src/baekjoon/sorting/K번째수_11004.java
+++ src/baekjoon/sorting/K번째수_11004.java
@@ -4,74 +4,62 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class K번째수_11004 {
-    // N개의 수들 int[] A
-    private static int[] A;
+    private static int[] numbers;
+    private static int K;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 수의 개수 N
         int N = Integer.parseInt(st.nextToken());
-        // 수의 위치 K
-        int K = Integer.parseInt(st.nextToken()) - 1;
+        K = Integer.parseInt(st.nextToken()) - 1;
         st = new StringTokenizer(br.readLine());
-        // A 초기화
-        A = new int[N];
+        numbers = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-        // 퀵 정렬
-        quicksort(1, N - 1, 0, K);
-        // K번째 있는 수 출력
-        bw.write(String.valueOf(A[K]));
+        quicksort(1, N - 1, 0);
+        bw.write(String.valueOf(numbers[K]));
         bw.flush();
     }
-    // 퀵 정렬
-    // 기준값 pivot을 정해 pivot보다 작은 데이터와 큰 데이터로 분류하여 정렬
-    private static void quicksort(int s, int e, int p, int k){
-        // 시작 위치가 끝 위치보다 클 경우
+    private static void quicksort(int s, int e, int p){
         if (s > e) {
             return;
         }
-        // 시작 위치
         int startIdx = s;
-        // 끝 위치
         int endIdx = e;
-        // 기준값 위치
         int pivotIdx = p;
-        // 작은데이터와 큰데이터 분류
         while (startIdx < endIdx) {
-            if (A[startIdx] > A[pivotIdx] && A[endIdx] < A[pivotIdx]) {
-                int temp = A[startIdx];
-                A[startIdx] = A[endIdx];
-                A[endIdx] = temp;
+            if (numbers[startIdx] > numbers[pivotIdx] && numbers[endIdx] < numbers[pivotIdx]) {
+                int temp = numbers[startIdx];
+                numbers[startIdx] = numbers[endIdx];
+                numbers[endIdx] = temp;
                 startIdx++;
                 endIdx--;
             } else {
-                if (A[startIdx] <= A[pivotIdx]) {
+                if (numbers[startIdx] <= numbers[pivotIdx]) {
                     startIdx++;
                 }
-                if (A[endIdx] >= A[pivotIdx]) {
+                if (numbers[endIdx] >= numbers[pivotIdx]) {
                     endIdx--;
                 }
             }
         }
-        // swap
-        int insertNum = A[pivotIdx];
-        int insertIdx = insertNum > A[startIdx] ? startIdx : startIdx - 1;
-        for (int i = pivotIdx; i < insertIdx; i++) {
-            A[i] = A[i + 1];
+        int insertIdx = pivotIdx;
+        if (numbers[pivotIdx] > numbers[endIdx]) {
+            int temp = numbers[pivotIdx];
+            for (int i = pivotIdx; i < endIdx; i++) {
+                numbers[i] = numbers[i + 1];
+            }
+            numbers[endIdx] = temp;
+            insertIdx = endIdx;
         }
-        A[insertIdx] = insertNum;
-
-        // 삽입한 위치가 k와 같다면 재귀 종료, 같지 않다면 재귀 호출
-        if (insertIdx == k) {
+        if (insertIdx == K) {
             return;
-        } else if (insertIdx > k) {
-            quicksort(s, insertIdx - 1, s - 1, k);
+        } else if (insertIdx > K) {
+            quicksort(s, endIdx - 1, s - 1);
         } else {
-            quicksort(insertIdx + 2, e, insertIdx + 1, k);
+            quicksort(insertIdx + 2, e, insertIdx + 1);
         }
     }
 }
