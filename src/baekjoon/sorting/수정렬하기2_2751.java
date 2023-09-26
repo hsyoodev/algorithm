1f01c3506142259fe0094026d76b98c64d9e4861
hsyoodev <hsyoodev@gmail.com>
2023-09-26 10:12:16 +0900

    cleaned 2751

--- src/baekjoon/sorting/수정렬하기2_2751.java
+++ src/baekjoon/sorting/수정렬하기2_2751.java
@@ -3,57 +3,42 @@ package baekjoon.sorting;
 import java.io.*;
 
 public class 수정렬하기2_2751 {
-    private static int[] A;
-    private static int[] M;
+    private static int[] numbers;
+    private static int[] temps;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        A = new int[N + 1];
-        M = new int[N + 1];
+        numbers = new int[N + 1];
+        temps = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            A[i] = Integer.parseInt(br.readLine());
+            numbers[i] = Integer.parseInt(br.readLine());
         }
         mergesort(1, N);
         for (int i = 1; i < N + 1; i++) {
-            bw.write(A[i] + "\n");
+            bw.write(numbers[i] + "\n");
         }
         bw.flush();
     }
-    private static void mergesort(int s, int e) {
-        if (s == e) {
+
+    private static void mergesort(int startIndex, int endIndex) {
+        if (startIndex == endIndex) {
             return;
         }
-
-        int s2 = ((s + e) / 2) + 1;
-        mergesort(s, s2 - 1);
-        mergesort(s2, e);
-
-        int mv1 = s;
-        int mv2 = s2;
-        int m = s;
-        while (m <= e) {
-            if (mv1 < s2 && mv2 <= e) {
-                if (A[mv1] >= A[mv2]) {
-                    M[m] = A[mv2];
-                    mv2++;
-                } else {
-                    M[m] = A[mv1];
-                    mv1++;
-                }
-            } else {
-                if (mv1 >= s2 && mv2 <= e) {
-                    M[m] = A[mv2];
-                    mv2++;
-                } else {
-                    M[m] = A[mv1];
-                    mv1++;
-                }
-            }
-            m++;
+        int splitEndIndex = ((startIndex + endIndex) / 2);
+        int splitStartIndex = splitEndIndex + 1;
+        mergesort(startIndex, splitEndIndex);
+        mergesort(splitStartIndex, endIndex);
+        int moveStartIndex1 = startIndex;
+        int moveStartIndex2 = splitStartIndex;
+        int tempsIndex = startIndex;
+        while (tempsIndex <= endIndex) {
+            boolean isStartIndex1 = moveStartIndex2 > endIndex || (moveStartIndex1 < splitStartIndex && numbers[moveStartIndex1] < numbers[moveStartIndex2]);
+            temps[tempsIndex++] = numbers[isStartIndex1 ? moveStartIndex1++ : moveStartIndex2++];
         }
-        for (int j = s; j <= e; j++) {
-            A[j] = M[j];
+        for (int j = startIndex; j <= endIndex; j++) {
+            numbers[j] = temps[j];
         }
     }
 }
