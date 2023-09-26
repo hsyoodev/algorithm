a6de5574629adc18dc724ba17e53c7416f432207
hsyoodev <hsyoodev@gmail.com>
2023-09-26 14:05:31 +0900

    checked 10989

--- src/baekjoon/sorting/수정렬하기3_10989.java
+++ src/baekjoon/sorting/수정렬하기3_10989.java
@@ -1,22 +1,40 @@
 package baekjoon.sorting;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class 수정렬하기3_10989 {
+    private static int[] numbers;
+    private static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[10001];
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
         for (int i = 0; i < N; i++) {
-            int index = Integer.parseInt(br.readLine());
-            A[index]++;
+            numbers[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 1; i < 10001; i++) {
-            while (A[i]-- != 0) {
-                bw.write(i + "\n");
-            }
+        radixsort();
+        for (int i = 0; i < N; i++) {
+            bw.write(numbers[i] + "\n");
         }
         bw.flush();
+    }
+
+    private static void radixsort() {
+        int[] outputs = new int[N];
+        for (int digit = 1; digit <= 10000; digit *= 10) {
+            int[] digitCounts = new int[10];
+            for (int i = 0; i < N; i++) {
+                digitCounts[(numbers[i] / digit) % 10]++;
+            }
+            Arrays.parallelPrefix(digitCounts, Integer::sum);
+            for (int i = N - 1; i >= 0; i--) {
+                outputs[digitCounts[(numbers[i] / digit) % 10] - 1] = numbers[i];
+                digitCounts[(numbers[i] / digit) % 10]--;
+            }
+            System.arraycopy(outputs, 0, numbers, 0, N);
+        }
     }
 }
