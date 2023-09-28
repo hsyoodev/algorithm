302d42a65df307974cf2c9a66745af847e698316
hsyoodev <hsyoodev@gmail.com>
2023-09-29 02:08:32 +0900

    checked 2178

--- src/baekjoon/BFS/미로탐색_2178.java
+++ src/baekjoon/BFS/미로탐색_2178.java
@@ -1,20 +1,21 @@
 package baekjoon.BFS;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class 미로탐색_2178 {
-    static int N;
-    static int M;
     static int[][] maze;
+    static boolean[][] visited;
+    static int[] directionX = {-1, 0, 1, 0};
+    static int[] directionY = {0, 1, 0, -1};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
         maze = new int[N + 2][M + 2];
+        visited = new boolean[N + 2][M + 2];
         for (int i = 1; i < N + 1; i++) {
             char[] numbers = scanner.next().toCharArray();
             for (int j = 1; j < M + 1; j++) {
@@ -22,27 +23,24 @@ public class 미로탐색_2178 {
             }
         }
         BFS(1, 1);
-
+        System.out.print(maze[N][M]);
     }
 
-    static void BFS(int rowIndex, int columnIndex) {
-        int current = maze[rowIndex][columnIndex];
-        int up = maze[rowIndex - 1][columnIndex];
-        int down = maze[rowIndex + 1][columnIndex];
-        int left = maze[rowIndex][columnIndex - 1];
-        int right = maze[rowIndex][columnIndex + 1];
-
-        if (up == 1 ) {
-            maze[rowIndex - 1][columnIndex] += current;
-        }
-        if (down == 1) {
-            maze[rowIndex + 1][columnIndex] += current;
-        }
-        if (left == 1) {
-            maze[rowIndex][columnIndex - 1] += current;
-        }
-        if (right == 1) {
-            maze[rowIndex][columnIndex + 1] += current;
+    static void BFS(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[x][y] = true;
+        queue.add(new int[]{x, y});
+        while (!queue.isEmpty()) {
+            int[] xy = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int targetX = xy[0] + directionX[i];
+                int targetY = xy[1] + directionY[i];
+                if (!visited[targetX][targetY] && maze[targetX][targetY] != 0) {
+                    maze[targetX][targetY] += maze[xy[0]][xy[1]];
+                    visited[targetX][targetY] = true;
+                    queue.add(new int[]{targetX, targetY});
+                }
+            }
         }
     }
 }
