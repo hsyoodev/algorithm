8baad5d106f5464bf200ab9323ef1fbb5611eabd
hsyoodev <hsyoodev@gmail.com>
2023-10-01 18:25:55 +0900

    cleaned 11724

--- src/baekjoon/DFS/연결요소의개수_11724.java
+++ src/baekjoon/DFS/연결요소의개수_11724.java
@@ -3,40 +3,41 @@ package baekjoon.DFS;
 import java.util.*;
 
 public class 연결요소의개수_11724 {
+    static ArrayList<Integer>[] adjacencyList;
+    static boolean[] visited;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int M = scanner.nextInt();
-        Queue<Integer>[] adjacencyList = new Queue[N + 1];
+        adjacencyList = new ArrayList[N + 1];
         for (int i = 1; i < N + 1; i++) {
-            adjacencyList[i] = new LinkedList<>();
+            adjacencyList[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
-            int node = scanner.nextInt();
-            int adjacencyPoint = scanner.nextInt();
-            adjacencyList[node].offer(adjacencyPoint);
-            adjacencyList[adjacencyPoint].offer(node);
+            int node1 = scanner.nextInt();
+            int node2 = scanner.nextInt();
+            adjacencyList[node1].add(node2);
+            adjacencyList[node2].add(node1);
         }
-        boolean[] visited = new boolean[N + 1];
-        Stack<Integer> stack = new Stack<>();
+        visited = new boolean[N + 1];
         int answer = 0;
         for (int i = 1; i < N + 1; i++) {
             if (!visited[i]) {
                 answer++;
-                stack.push(i);
-                visited[i] = true;
-                while (!stack.isEmpty()) {
-                    int node = stack.pop();
-                    while (!adjacencyList[node].isEmpty()) {
-                        int adjacencyPoint = adjacencyList[node].poll();
-                        if (!visited[adjacencyPoint]) {
-                            stack.push(adjacencyPoint);
-                            visited[adjacencyPoint] = true;
-                        }
-                    }
-                }
+                DFS(i);
             }
         }
         System.out.print(answer);
+    }
+    static void DFS(int node) {
+        if (visited[node]) {
+            return;
+        }
+        visited[node] = true;
+        for (int adjacencyNode : adjacencyList[node]) {
+            if (!visited[adjacencyNode]) {
+                DFS(adjacencyNode);
+            }
+        }
     }
 }
