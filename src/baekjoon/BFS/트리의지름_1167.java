185c78b8c6e3bd9fe7a6b1377b805ebf832b2361
hsyoodev <hsyoodev@gmail.com>
2023-10-03 16:12:20 +0900

    checked 1167

--- src/baekjoon/BFS/트리의지름_1167.java
+++ src/baekjoon/BFS/트리의지름_1167.java
@@ -3,21 +3,19 @@ package baekjoon.BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 트리의지름_1167 {
     static ArrayList<Node>[] adjacencyList;
     static boolean[] visited;
-    static int treeDiameter;
+    static int[] distance;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int V = Integer.parseInt(br.readLine());
         adjacencyList = new ArrayList[V + 1];
         visited = new boolean[V + 1];
+        distance = new int[V + 1];
         for (int i = 1; i < V + 1; i++) {
             adjacencyList[i] = new ArrayList<>();
         }
@@ -35,8 +33,16 @@ public class 트리의지름_1167 {
                 adjacencyList[node2].add(new Node(node1, distance));
             }
         }
-        BFS(1);
-        System.out.print(treeDiameter);
+        int vertexNumber = 1;
+        BFS(vertexNumber);
+        for (int i = 1; i < V + 1; i++) {
+            if (distance[vertexNumber] < distance[i]) {
+                vertexNumber = i;
+            }
+        }
+        Arrays.fill(visited, false);
+        BFS(vertexNumber);
+        System.out.print(Arrays.stream(distance).max().getAsInt());
     }
 
     static void BFS(int vertexNumber) {
@@ -45,10 +51,15 @@ public class 트리의지름_1167 {
         visited[vertexNumber] = true;
         while (!queue.isEmpty()) {
             Node currentNode = queue.poll();
-            for (Node adjacencyNode : adjacencyList[currentNode.vertexNumber]) {
-                if (!visited[adjacencyNode.vertexNumber]) {
-                    queue.offer(adjacencyNode);
-                    visited[adjacencyNode.vertexNumber] = true;
+            int currentVertexNumber = currentNode.vertexNumber;
+            int currentDistance = currentNode.distance;
+            distance[currentVertexNumber] = currentDistance;
+            for (Node adjacencyNode : adjacencyList[currentVertexNumber]) {
+                int adjacencyVertexNumber = adjacencyNode.vertexNumber;
+                int adjacencyDistance = adjacencyNode.distance;
+                if (!visited[adjacencyVertexNumber]) {
+                    queue.offer(new Node(adjacencyVertexNumber, adjacencyDistance + currentDistance));
+                    visited[adjacencyVertexNumber] = true;
                 }
             }
         }
