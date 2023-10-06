824037aabed8ab5331522fe151e645fd78c93999
hsyoodev <hsyoodev@gmail.com>
2023-10-06 14:28:57 +0900

    unsolved 2343

--- src/baekjoon/binary_search/기타레슨_2343.java
+++ src/baekjoon/binary_search/기타레슨_2343.java
@@ -8,12 +8,13 @@ import java.util.StringTokenizer;
 
 public class 기타레슨_2343 {
     static int[] numbers;
+    static int M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         numbers = new int[N + 1];
         st = new StringTokenizer(br.readLine());
         int max = -1;
@@ -24,16 +25,19 @@ public class 기타레슨_2343 {
             }
         }
         Arrays.parallelPrefix(numbers, Integer::sum);
-        for (int i = 1; i < N; i++) {
-            if (numbers[i] >= max) {
-
+        for (int i = 1; i < N - M; i++) {
+            if (numbers[i] > max) {
+                partition(i, N, 1);
             }
         }
-
-
     }
 
-    static void binarySearch(int startIndex, int endIndex) {
+    static void partition(int startIndex, int endIndex, int depth) {
+        if (depth == M - 1) {
+            return;
+        }
+        int middleIndex = (startIndex + endIndex) / 2;
+
 
 
 
