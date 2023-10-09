fbcc7ec4193e57b764b22558a7da645289261848
hsyoodev <hsyoodev@gmail.com>
2023-10-09 23:24:50 +0900

    unsolved 1300

---
+++ src/baekjoon/binary_search/K번째수_1300.java
@@ -0,0 +1,11 @@
+package baekjoon.binary_search;
+
+import java.util.Scanner;
+
+public class K번째수_1300 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int k = scanner.nextInt();
+    }
+}
