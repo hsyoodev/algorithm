56f33f7199a6f51a53126bd07d23787f43b11df9
hsyoodev <hsyoodev@gmail.com>
2023-10-12 15:26:25 +0900

    unsolved 2001

---
+++ src/SWEA/D2/파리퇴치_2001.java
@@ -0,0 +1,25 @@
+package SWEA.D2;
+
+import java.util.Scanner;
+
+public class 파리퇴치_2001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int[][] flyArea = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    flyArea[i][j] = sc.nextInt();
+                }
+            }
+            int max = 0;
+            for (int i = 0; i < N; i++) {
+
+            }
+        }
+    }
+
+}
