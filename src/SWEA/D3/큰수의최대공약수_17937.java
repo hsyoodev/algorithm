fcd67e677abfad1875e79572653a8757bd3132f5
hsyoodev <hsyoodev@gmail.com>
2023-10-16 14:41:07 +0900

    modified 17937

--- src/SWEA/D3/큰수의최대공약수_17937.java
+++ src/SWEA/D3/큰수의최대공약수_17937.java
@@ -10,22 +10,15 @@ public class 큰수의최대공약수_17937 {
         for (int test_case = 1; test_case <= T; test_case++) {
             BigInteger A = sc.nextBigInteger();
             BigInteger B = sc.nextBigInteger();
-            BigInteger max = BigInteger.valueOf(0);
-            for (BigInteger i = BigInteger.valueOf(1); i.multiply(i).compareTo(A) != 1; i = i.add(BigInteger.valueOf(1))) {
-                boolean isGCD = true;
-                for (BigInteger j = A; j.compareTo(B) != 1; j = j.add(BigInteger.valueOf(1))) {
-                    if (j.mod(i) != BigInteger.valueOf(0)) {
-                        isGCD = false;
-                        break;
-                    }
-                }
-                if (isGCD) {
-                    if (max.compareTo(i) == -1) {
-                        max = i;
-                    }
-                }
+            BigInteger temp = A;
+            A= B.mod(A);
+            B = temp;
+            while (A.compareTo(BigInteger.valueOf(0)) != 0) {
+                temp = A;
+                A = B.mod(A);
+                B = temp;
             }
-            System.out.printf("#%d %d\n", test_case, max);
+            System.out.printf("#%d %d\n", test_case, B);
         }
     }
 }
