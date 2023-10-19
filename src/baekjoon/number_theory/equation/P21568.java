bc319f389a2097e09078d08d52e5e3dd1eadc047
hsyoodev <hsyoodev@gmail.com>
2023-10-20 00:03:16 +0900

    unsolved 21568

--- src/baekjoon/number_theory/equation/P21568.java
+++ src/baekjoon/number_theory/equation/P21568.java
@@ -1,33 +1,41 @@
 package baekjoon.number_theory.equation;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class P21568 {
-    static ArrayList<>
+    static Stack<Integer> quotients = new Stack<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int A = scanner.nextInt();
         int B = scanner.nextInt();
         int C = scanner.nextInt();
         int gcd = GCD(A, B);
+        int x = 1;
+        int y = 0;
+        while (!quotients.isEmpty()) {
+            int q = quotients.pop();
+            int prevX = x;
+            int prevY = y;
+            x = prevY;
+            y = prevX - (prevY * q);
+        }
+        int K = C / gcd;
+        x *= K;
+        y *= K;
+        boolean isAxByC = A * x + B * y == C;
+        System.out.print(isAxByC ? x + " " + y : -1);
     }
 
     public static int GCD(int A, int B) {
-        while (A % B != 0) {
+        while (B != 0) {
+            int quotient = A / B;
+            quotients.push(quotient);
             int temp = B;
             B = A % B;
             A = temp;
         }
         return A;
-    }
-}
-
-class QuotientAndRemainder {
-    public int quotient;
-    public int remainder;
-    public QuotientAndRemainder(int quotient, int remainder) {
-        this.quotient = quotient;
-        this.remainder = remainder;
     }
 }
