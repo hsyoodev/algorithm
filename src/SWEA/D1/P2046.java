0ae692cef95220add2ab39c716726f2b02ae4681
hsyoodev <hsyoodev@gmail.com>
2023-10-23 15:44:40 +0900

    solved 2046

---
+++ src/SWEA/D1/P2046.java
@@ -0,0 +1,13 @@
+package SWEA.D1;
+
+import java.util.Scanner;
+
+public class P2046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 0; i < number; i++) {
+            System.out.print("#");
+        }
+    }
+}
