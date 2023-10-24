937ad0208b47b5543bebd514b4aa055ccc9c20f9
hsyoodev <hsyoodev@gmail.com>
2023-10-24 21:03:20 +0900

    cleaned 11047

--- src/baekjoon/greedy/동전0_11047.java
+++ src/baekjoon/greedy/동전0_11047.java
@@ -3,19 +3,20 @@ package baekjoon.greedy;
 import java.util.Scanner;
 
 public class 동전0_11047 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        int[] A = new int[N];
+        int[] coinValue = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            coinValue[i] = sc.nextInt();
         }
         int count = 0;
-        for (int i = N - 1; i >=  0; i--) {
-            if (A[i] <= K) {
-                count += K / A[i];
-                K %= A[i];
+        for (int i = N - 1; i >= 0; i--) {
+            if (coinValue[i] <= K) {
+                count += K / coinValue[i];
+                K %= coinValue[i];
             }
         }
         System.out.print(count);
