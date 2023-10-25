df9b9cea5c41828e22a0913e5a3fec827097c6c4
hsyoodev <hsyoodev@gmail.com>
2023-10-25 22:51:27 +0900

    modified 1300

--- src/baekjoon/binary_search/K번째수_1300.java
+++ src/baekjoon/binary_search/K번째수_1300.java
@@ -3,6 +3,7 @@ package baekjoon.binary_search;
 import java.util.Scanner;
 
 public class K번째수_1300 {
+
     static int N;
     static int K;
 
@@ -10,28 +11,10 @@ public class K번째수_1300 {
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
         K = scanner.nextInt();
-        binarySearchX(1, N);
+        binarySearch(1, N);
     }
 
-    static void binarySearchX(int x1, int x2) {
-        int midX = (x1 + x2) / 2;
-        int prevX = midX - 1;
-        int nextX = midX + 1;
-        int minCount = (N * 2 * prevX) - (prevX * prevX) + 1;
-        int maxCount = minCount - 1 + (N - midX + 1) * 2 - 1;
-        if (K > maxCount) {
-            binarySearchX(nextX, x2);
-        } else if (K < minCount) {
-            binarySearchX(x1, prevX);
-        } else {
-            int count = minCount;
-            for (int i = midX; i <= N; i++) {
-                if (K <= count) {
-                    System.out.print(midX * i);
-                    break;
-                }
-                count += 2;
-            }
-        }
+    private static void binarySearch(int start, int end) {
+        
     }
 }
