50d4f5769a9da1fde8886d88cfccf439eaa28584
hsyoodev <hsyoodev@gmail.com>
2024-04-26 17:43:46 +0900

    solved P10816

---
+++ src/baekjoon/data_structures/P10816.java
@@ -0,0 +1,33 @@
+package baekjoon.data_structures;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/10816
+public class P10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<Integer, Integer> map = new HashMap<>();
+        // 숫자 카드의 개수
+        int N = Integer.parseInt(bufferedReader.readLine());
+        // 숫자 카드에 적혀있는 정수 갯수
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(stringTokenizer.nextToken());
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+        // 정수의 개수
+        int M = Integer.parseInt(bufferedReader.readLine());
+        // 각 수가 적힌 숫자 카드를 몇 개 가지고 있는지
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < M; i++) {
+            int number = Integer.parseInt(stringTokenizer.nextToken());
+            bufferedWriter.write(!map.containsKey(number) ? "0" : String.valueOf(map.get(number)));
+            bufferedWriter.write(" ");
+        }
+        bufferedWriter.flush();
+    }
+}
