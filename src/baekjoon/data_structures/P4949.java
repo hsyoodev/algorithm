2eeadb06045855a5e26cb4f60189a693a4144f78
hsyoodev <hsyoodev@gmail.com>
2024-04-30 08:57:53 +0900

    refactored P4949

--- src/baekjoon/data_structures/P4949.java
+++ src/baekjoon/data_structures/P4949.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+// https://www.acmicpc.net/problem/4949
 public class P4949 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
