2e335767517671aa57999ac630748fd966cb3a50
hsyoodev <hsyoodev@gmail.com>
2024-04-30 09:02:40 +0900

    refactored P4949

--- src/baekjoon/data_structures/P4949.java
+++ src/baekjoon/data_structures/P4949.java
@@ -11,31 +11,31 @@ public class P4949 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             Stack<String> stack = new Stack<>();
-            String str = bufferedReader.readLine();
+            String string = bufferedReader.readLine();
             // 입력의 종료조건으로 맨 마지막에 온점 하나(".")가 들어온다.
-            if (str.equals(".")) {
+            if (string.equals(".")) {
                 break;
             }
             // 괄호들의 균형이 잘 맞춰져 있는지 판단
-            String[] strs = str.split("");
-            for (int i = 0; i < strs.length; i++) {
-                switch (strs[i]) {
+            String[] strs = string.split("");
+            for (String str : strs) {
+                switch (str) {
                     case "(":
                     case "[":
-                        stack.push(strs[i]);
+                        stack.push(str);
                         break;
                     case ")":
                         if (!stack.isEmpty() && stack.peek().equals("(")) {
                             stack.pop();
                         } else {
-                            stack.push(strs[i]);
+                            stack.push(str);
                         }
                         break;
                     case "]":
                         if (!stack.isEmpty() && stack.peek().equals("[")) {
                             stack.pop();
                         } else {
-                            stack.push(strs[i]);
+                            stack.push(str);
                         }
                         break;
                 }
