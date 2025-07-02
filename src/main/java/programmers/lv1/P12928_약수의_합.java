504cebd544b8e6653bf7e998adff6d68ed59bd7b
hsyoo <hsyoodev@icloud.com>
2025-07-02 21:16:25 +0900

    solved P12928_약수의_합

---
+++ src/main/java/programmers/lv1/P12928_약수의_합.java
@@ -0,0 +1,22 @@
+package programmers.lv1;
+
+import java.util.stream.IntStream;
+
+public class P12928_약수의_합 {
+
+    public static void main(String[] args) {
+        System.out.println("n : 12, return : " + solution(12));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
