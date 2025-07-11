11892396c8551214e1cfb1b9989f668d384661f0
hsyoo <hsyoodev@icloud.com>
2025-07-12 00:14:34 +0900

    refactor P12931_자릿수_더하기

--- src/main/java/programmers/lv1/P12931_자릿수_더하기.java
+++ src/main/java/programmers/lv1/P12931_자릿수_더하기.java
@@ -4,7 +4,7 @@ public class P12931_자릿수_더하기 {
 
     public static void main(String[] args) {
         System.out.println(solution(123));
-        System.out.println(solution(987));
+        System.out.print(solution(987));
     }
 
     public static int solution(int n) {
