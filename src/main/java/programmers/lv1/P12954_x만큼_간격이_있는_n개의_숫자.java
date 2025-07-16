66d63cade38d6fa843b55aedf8f317c693698ef1
hsyoo <hsyoodev@icloud.com>
2025-07-16 23:46:51 +0900

    solve P12954_x만큼_간격이_있는_n개의_숫자

---
+++ src/main/java/programmers/lv1/P12954_x만큼_간격이_있는_n개의_숫자.java
@@ -0,0 +1,21 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class P12954_x만큼_간격이_있는_n개의_숫자 {
+
+    public static void main(String[] args) {
+        System.out.print(Arrays.toString(solution(2, 5)));
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 1; i <= n; i++) {
+            answer[i - 1] = Long.parseLong(String.valueOf(x)) * i;
+        }
+
+        return answer;
+    }
+
+}
