1e453a9eb2f0fc1f47ac6e1c56ab9b5e5fe2f6f2
hsyoo <hsyoodev@icloud.com>
2025-07-18 23:28:48 +0900

    solve P12925_문자열을_정수로_바꾸기

---
+++ src/main/java/programmers/lv1/P12925_문자열을_정수로_바꾸기.java
@@ -0,0 +1,13 @@
+package programmers.lv1;
+
+public class P12925_문자열을_정수로_바꾸기 {
+
+    public static void main(String[] args) {
+        System.out.print(solution("-1234"));
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+}
