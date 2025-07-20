af95313c34bcd06478ae1b6eb5160e19d45c0e8a
hsyoo <hsyoodev@icloud.com>
2025-07-20 22:25:35 +0900

    refactor P12912_두_정수_사이의_합

--- src/main/java/programmers/lv1/P12912_두_정수_사이의_합.java
+++ src/main/java/programmers/lv1/P12912_두_정수_사이의_합.java
@@ -1,7 +1,5 @@
 package programmers.lv1;
 
-import java.util.stream.LongStream;
-
 public class P12912_두_정수_사이의_합 {
 
     public static void main(String[] args) {
@@ -9,10 +7,7 @@ public class P12912_두_정수_사이의_합 {
     }
 
     public static long solution(int a, int b) {
-        int seed = Math.min(a, b);
-        int maxSize = Math.abs(b - a) + 1;
-
-        return LongStream.iterate(seed, f -> f + 1).limit(maxSize).sum();
+        return (long) (b - a + 1) * (a + b) / 2;
     }
 
 }
