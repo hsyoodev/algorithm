6279bdb66a33f9618cc518482f24893e79391ce9
hsyoo <hsyoodev@icloud.com>
2025-07-21 23:12:52 +0900

    refactor P12916_문자열_내_p와_y의_개수

--- src/main/java/programmers/lv1/P12916_문자열_내_p와_y의_개수.java
+++ src/main/java/programmers/lv1/P12916_문자열_내_p와_y의_개수.java
@@ -1,21 +1,14 @@
 package programmers.lv1;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class P12916_문자열_내_p와_y의_개수 {
 
     public static void main(String[] args) {
-        System.out.print(solution("Pyy"));
+        System.out.print(solution("pPoooyY"));
     }
 
     public static boolean solution(String s) {
-        Map<Character, Integer> alphabetCounts = new HashMap<>();
+        s = s.toLowerCase();
 
-        for (char alphabet : s.toLowerCase().toCharArray()) {
-            alphabetCounts.put(alphabet, alphabetCounts.getOrDefault(alphabet, 0) + 1);
-        }
-
-        return alphabetCounts.get('p') == alphabetCounts.get('y');
+        return s.chars().filter(c -> c == 'p').count() == s.chars().filter(c -> c == 'y').count();
     }
 }
