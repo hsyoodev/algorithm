0e571b359397b733fc1aa15547d9d9348f4c879e
hsyoo <hsyoodev@icloud.com>
2025-07-22 23:46:25 +0900

    refactor P12934_정수_제곱근_판별

--- src/main/java/programmers/lv1/P12934_정수_제곱근_판별.java
+++ src/main/java/programmers/lv1/P12934_정수_제곱근_판별.java
@@ -3,16 +3,12 @@ package programmers.lv1;
 public class P12934_정수_제곱근_판별 {
 
     public static void main(String[] args) {
-        System.out.print(solution(121));
+        System.out.print(solution(3));
     }
 
     public static long solution(long n) {
-        long answer = 1;
-
-        for (answer = 1; answer * answer <= n; answer++) {
-            if (answer * answer == n) {
-                return (answer + 1) * (answer + 1);
-            }
+        if ((int) Math.pow(Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
         }
 
         return -1;
