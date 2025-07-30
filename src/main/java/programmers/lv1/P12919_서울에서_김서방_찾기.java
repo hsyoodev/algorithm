79f37f82240a83293ec899c382d85e0a190b2c0f
hsyoo <hsyoodev@icloud.com>
2025-07-30 23:43:29 +0900

    solve P12919_서울에서_김서방_찾기

---
+++ src/main/java/programmers/lv1/P12919_서울에서_김서방_찾기.java
@@ -0,0 +1,21 @@
+package programmers.lv1;
+
+public class P12919_서울에서_김서방_찾기 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"Jane", "Kim"}));
+    }
+
+    public static String solution(String[] seoul) {
+        String answer = "";
+
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+            }
+        }
+
+        return answer;
+    }
+
+}
