b6a5b87d97072257f4487c45a577a87b449e3129
hsyoo <hsyoodev@icloud.com>
2025-07-30 23:45:36 +0900

    refactor P12919_서울에서_김서방_찾기

--- src/main/java/programmers/lv1/P12919_서울에서_김서방_찾기.java
+++ src/main/java/programmers/lv1/P12919_서울에서_김서방_찾기.java
@@ -1,5 +1,7 @@
 package programmers.lv1;
 
+import java.util.List;
+
 public class P12919_서울에서_김서방_찾기 {
 
     public static void main(String[] args) {
@@ -7,15 +9,7 @@ public class P12919_서울에서_김서방_찾기 {
     }
 
     public static String solution(String[] seoul) {
-        String answer = "";
-
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) {
-                answer = "김서방은 " + i + "에 있다";
-            }
-        }
-
-        return answer;
+        return "김서방은 " + List.of(seoul).indexOf("Kim") + "에 있다";
     }
 
 }
