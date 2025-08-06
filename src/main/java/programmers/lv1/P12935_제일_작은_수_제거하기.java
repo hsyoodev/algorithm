9232c41a509677b9816412e9580ef932bc030b20
hsyoo <hsyoodev@icloud.com>
2025-08-06 22:04:06 +0900

    solve P12935_제일_작은_수_제거하기

---
+++ src/main/java/programmers/lv1/P12935_제일_작은_수_제거하기.java
@@ -0,0 +1,19 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class P12935_제일_작은_수_제거하기 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{4, 3, 2, 1})));
+        System.out.print(Arrays.toString(solution(new int[]{10})));
+    }
+
+    public static int[] solution(int[] arr) {
+        int min = Arrays.stream(arr).min().getAsInt();
+        int[] answer = Arrays.stream(arr).filter(factor -> factor != min).toArray();
+
+        return answer.length != 0 ? answer : new int[]{-1};
+    }
+
+}
