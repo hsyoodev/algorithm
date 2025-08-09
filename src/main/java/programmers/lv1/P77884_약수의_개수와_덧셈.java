4d9274b0bed3c44862c572dee2a824c3f8054666
hsyoo <hsyoodev@icloud.com>
2025-08-09 15:16:07 +0900

    refactor P77884_약수의_개수와_덧셈

--- src/main/java/programmers/lv1/P77884_약수의_개수와_덧셈.java
+++ src/main/java/programmers/lv1/P77884_약수의_개수와_덧셈.java
@@ -1,8 +1,5 @@
 package programmers.lv1;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class P77884_약수의_개수와_덧셈 {
 
     public static void main(String[] args) {
@@ -13,16 +10,7 @@ public class P77884_약수의_개수와_덧셈 {
         int answer = 0;
 
         for (int i = left; i <= right; i++) {
-            Set<Integer> divisors = new HashSet<>();
-
-            for (int j = 1; j <= (i + 1) / 2; j++) {
-                if (i % j == 0) {
-                    divisors.add(j);
-                    divisors.add(i / j);
-                }
-            }
-
-            answer += divisors.size() % 2 == 0 ? i : -i;
+            answer += (i % Math.sqrt(i) == 0) ? -i : i;
         }
 
         return answer;
