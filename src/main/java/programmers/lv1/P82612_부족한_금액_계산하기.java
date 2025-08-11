170652ca5040a7158f2b66b62c5fa7b02d18e007
hsyoo <hsyoodev@icloud.com>
2025-08-12 00:31:55 +0900

    refactor P82612_부족한_금액_계산하기

--- src/main/java/programmers/lv1/P82612_부족한_금액_계산하기.java
+++ src/main/java/programmers/lv1/P82612_부족한_금액_계산하기.java
@@ -1,15 +1,13 @@
 package programmers.lv1;
 
-import java.util.stream.LongStream;
-
 public class P82612_부족한_금액_계산하기 {
 
     public static void main(String[] args) {
         System.out.print(solution(3, 20, 4));
     }
 
-    public static long solution(int price, int money, int count) {
-        return Math.max(0, LongStream.range(1, count + 1).map(i -> i * price).sum() - money);
+    public static long solution(long price, long money, long count) {
+        return Math.max(0, count * (price + (price * count)) / 2 - money);
     }
 
 }
