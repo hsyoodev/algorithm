d90b1fd79391556ba47adf54c9ec2fa5fe0592bf
hsyoo <hsyoodev@icloud.com>
2025-08-19 23:11:10 +0900

    solve P12982_예산

---
+++ src/main/java/programmers/lv1/P12982_예산.java
@@ -0,0 +1,30 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class P12982_예산 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 3, 2, 5, 4}, 9));
+        System.out.print(solution(new int[]{2, 2, 3, 3}, 10));
+    }
+
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            if (budget < d[i]) {
+                break;
+            }
+
+            budget -= d[i];
+
+            answer++;
+        }
+
+        return answer;
+    }
+
+}
