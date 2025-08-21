5005ff30597941fdbf3fc00bf6bbf7e7eed30be5
hsyoo <hsyoodev@icloud.com>
2025-08-22 00:00:41 +0900

    refactor P131705_삼총사

--- src/main/java/programmers/lv1/P131705_삼총사.java
+++ src/main/java/programmers/lv1/P131705_삼총사.java
@@ -1,12 +1,7 @@
 package programmers.lv1;
 
-import java.util.Arrays;
-
 public class P131705_삼총사 {
-
-    static boolean[] isVisited;
-    static int answer;
-
+    
     public static void main(String[] args) {
         System.out.println(solution(new int[]{-2, 3, 0, 2, -5}));
         System.out.println(solution(new int[]{-3, -2, -1, 0, 1, 2, 3}));
@@ -14,36 +9,19 @@ public class P131705_삼총사 {
     }
 
     public static int solution(int[] number) {
-        answer = 0;
-        isVisited = new boolean[number.length];
+        int answer = 0;
 
         for (int i = 0; i < number.length; i++) {
-            DFS(number, i, 1, number[i]);
-
-            Arrays.fill(isVisited, false);
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
         }
 
         return answer;
-    }
-
-    public static void DFS(int[] number, int v, int depth, int sum) {
-        isVisited[v] = true;
-
-        if (depth == 3) {
-            if (sum == 0) {
-                answer++;
-            }
-
-            isVisited[v] = false;
-
-            return;
-        }
-
-        for (int i = v + 1; i < number.length; i++) {
-            if (!isVisited[i]) {
-                DFS(number, i, depth + 1, sum + number[i]);
-            }
-        }
     }
 
 }
