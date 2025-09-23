1ff6b2bfccd3346b9fb00703a0e0639fd5b3942b
hsyoo <hsyoodev@icloud.com>
2025-09-23 23:39:55 +0900

    unsolve P12915_문자열_내_마음대로_정렬하기

---
+++ src/main/java/programmers/lv1/P12915_문자열_내_마음대로_정렬하기.java
@@ -0,0 +1,27 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class P12915_문자열_내_마음대로_정렬하기 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new String[]{"sun", "bed", "car"}, 1)));
+        System.out.print(Arrays.toString(solution(new String[]{"abce", "abcd", "cdx"}, 2)));
+    }
+
+    public static String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (s1, s2) -> {
+            char c1 = s1.charAt(n);
+            char c2 = s2.charAt(n);
+
+            if (c1 == c2) {
+                return s1.compareTo(s2);
+            }
+
+            return Character.compare(c1, c2);
+        });
+
+        return strings;
+    }
+
+}
