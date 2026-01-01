4a8d7967659919125dc7cd230a995806b964ba0e
hsyoo <hsyoodev@icloud.com>
2026-01-01 17:05:56 +0900

    retried P13023_ABCDE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    private static ArrayList<Integer>[] graph;
+    private static boolean[] isVisited;
+    private static boolean isExist = false;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
-        for (int i = 2; i <= 9; i++) {
-            if (isPrimeNumber(i)) {
-                DFS(i, N);
-            }
+        graph = new ArrayList[N];
+        isVisited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
         }
+
+        for (int i = 0; i < M; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 0; i < N; i++) {
+            DFS(i, 1);
+
+            if (isExist) {
+                break;
+            }
+
+            Arrays.fill(isVisited, false);
+        }
+
+        System.out.print(isExist ? 1 : 0);
     }
 
-    private static void DFS(int number, int length) {
-        if (String.valueOf(number).length() == length) {
-            System.out.println(number);
+    private static void DFS(int node, int depth) {
+        if (depth == 5) {
+            isExist = true;
 
             return;
         }
 
-        for (int i = 1; i <= 9; i++) {
-            int nextNumber = Integer.parseInt(String.valueOf(number) + i);
+        isVisited[node] = true;
 
-            if (isPrimeNumber(nextNumber)) {
-                DFS(nextNumber, length);
+        for (int i = 0; i < graph[node].size(); i++) {
+            int nextNode = graph[node].get(i);
+
+            if (!isVisited[nextNode]) {
+                DFS(nextNode, depth + 1);
+
+                isVisited[nextNode] = false;
             }
         }
-    }
-
-    private static boolean isPrimeNumber(int number) {
-        for (int i = 2; i * i <= number; i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
 }
