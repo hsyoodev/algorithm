a1cb667423bd308ced687663ab76f1f6a2958c13
hsyoo <hsyoodev@icloud.com>
2026-02-03 00:08:19 +0900

    retried P2178_미로_탐색

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,61 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-    private static ArrayList<Integer>[] graph;
-    private static boolean[] isVisited;
-    private static boolean isExist = false;
+    private static int N;
+    private static int M;
+    private static int[][] miro;
+    private static boolean[][] isVisited;
+    private static int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
+        N = scanner.nextInt();
+        M = scanner.nextInt();
 
-        graph = new ArrayList[N];
-        isVisited = new boolean[N];
-
-        for (int i = 0; i < N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < M; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            graph[a].add(b);
-            graph[b].add(a);
-        }
+        miro = new int[N][M];
+        isVisited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            DFS(i, 1);
+            String st = scanner.next();
 
-            if (isExist) {
-                break;
+            for (int j = 0; j < M; j++) {
+                miro[i][j] = Character.getNumericValue(st.charAt(j));
             }
-
-            Arrays.fill(isVisited, false);
         }
 
-        System.out.print(isExist ? 1 : 0);
+        BFS(0, 0);
+
+        System.out.print(miro[N - 1][M - 1]);
+
+        scanner.close();
     }
 
-    private static void DFS(int node, int depth) {
-        if (depth == 5) {
-            isExist = true;
+    private static void BFS(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
 
-            return;
-        }
+        queue.add(new int[]{x, y});
+        isVisited[x][y] = true;
 
-        isVisited[node] = true;
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
 
-        for (int i = 0; i < graph[node].size(); i++) {
-            int nextNode = graph[node].get(i);
+            for (int i = 0; i < 4; i++) {
+                int[] direction = directions[i];
+                int nextX = node[0] + direction[0];
+                int nextY = node[1] + direction[1];
 
-            if (!isVisited[nextNode]) {
-                DFS(nextNode, depth + 1);
+                if (nextX > -1 && nextX < N && nextY > -1 && nextY < M) {
+                    if (miro[nextX][nextY] == 1 && !isVisited[nextX][nextY]) {
+                        queue.add(new int[]{nextX, nextY});
+                        isVisited[nextX][nextY] = true;
 
-                isVisited[nextNode] = false;
+                        miro[nextX][nextY] += miro[node[0]][node[1]];
+                    }
+                }
             }
         }
     }
