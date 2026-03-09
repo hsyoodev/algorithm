a38e58d5e58b9210cb7e738aa4c21338416912ec
hsyoo <hsyoodev@icloud.com>
2026-03-09 21:42:48 +0900

    unsolved P11689_GCD
    문제가 너무 수학적이라 통과하자...

---
+++ src/main/java/baekjoon/gold/P11689_GCD.java
@@ -0,0 +1,19 @@
+package baekjoon.gold;
+
+public class P11689_GCD {
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        long n = scanner.nextLong();
+//        long answer = n;
+//
+//        for (long i = 2; i <= Math.sqrt(n); i++) {
+//            if (answer % i == 0) {
+//                answer -= (answer / i);
+//            }
+//        }
+
+//        System.out.print(answer);
+    }
+
+}
