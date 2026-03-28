2bc6191187470a9c94fe4d2f2f93ab89a9260430
hsyoo <hsyoodev@icloud.com>
2026-03-28 14:54:32 +0900

    retried P18352_특정_거리의_도시_찾기

--- src/main/java/baekjoon/silver/P18352_특정_거리의_도시_찾기.java
+++ src/main/java/baekjoon/silver/P18352_특정_거리의_도시_찾기.java
@@ -1,82 +1,74 @@
 package baekjoon.silver;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class P18352_특정_거리의_도시_찾기 {
 
-    private static int K;
-    private static boolean[] isVisited;
-    private static ArrayList<Node>[] graph;
-    private static final ArrayList<Integer> answer = new ArrayList<>();
+    private static int[] visited;
+    private static ArrayList<Integer>[] graph;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt(); // 도시의 개수
+        int M = scanner.nextInt(); // 도로의 개수
+        int K = scanner.nextInt(); // 거리 정보
+        int X = scanner.nextInt(); // 출발 도시의 번호
 
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-        K = scanner.nextInt();
-        int X = scanner.nextInt();
+        // 1.인접리스트, 방문기록 생성하기
         graph = new ArrayList[N + 1];
-        isVisited = new boolean[N + 1];
+        visited = new int[N + 1];
 
         for (int i = 1; i < N + 1; i++) {
-            graph[i] = new ArrayList<Node>();
+            graph[i] = new ArrayList<>();
+            visited[i] = -1;
         }
 
         for (int i = 0; i < M; i++) {
             int A = scanner.nextInt();
             int B = scanner.nextInt();
 
-            graph[A].add(new Node(B, 0));
+            graph[A].add(B);
         }
 
+        // 2.최단 거리 구하기
         BFS(X);
 
-        Collections.sort(answer);
+        // 3.정답 출력하기
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 1; i < N + 1; i++) {
+            if (visited[i] == K) {
+                answer.add(i);
+            }
+        }
 
         if (answer.isEmpty()) {
             System.out.print(-1);
         } else {
             answer.forEach(System.out::println);
         }
-
     }
 
-    private static void BFS(int vertex) {
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.add(new Node(vertex, 0));
-        isVisited[vertex] = true;
+    private static void BFS(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(node);
+        visited[node] = 0;
 
         while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
+            int nowNode = queue.poll();
 
-            if (currentNode.distance == K) {
-                answer.add(currentNode.vertex);
-            }
+            for (int i = 0; i < graph[nowNode].size(); i++) {
+                int nextNode = graph[nowNode].get(i);
 
-            for (int i = 0; i < graph[currentNode.vertex].size(); i++) {
-                Node nextNode = graph[currentNode.vertex].get(i);
-
-                if (!isVisited[nextNode.vertex]) {
-                    nextNode.distance = currentNode.distance + 1;
+                if (visited[nextNode] == -1) {
                     queue.add(nextNode);
-                    isVisited[nextNode.vertex] = true;
+                    visited[nextNode] = visited[nowNode] + 1;
                 }
             }
         }
-    }
-
-    private static class Node {
-
-        int vertex;
-        int distance;
-
-        public Node(int vertex, int distance) {
-            this.vertex = vertex;
-            this.distance = distance;
-        }
-
     }
 
 }
